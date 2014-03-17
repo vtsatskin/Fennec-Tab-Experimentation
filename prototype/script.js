9fbcf9d8c113b71d278810432e21730ddbc46b7b
Valentin Tsatskin <vtsatskin@mozilla.com>
2014-03-17 11:25:27 -0700

    add tab switching

--- prototype/script.js
+++ prototype/script.js
@@ -2,12 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
   var openTabButton = document.querySelector(".open-tabs");
   var menuButton = document.querySelector(".menu");
   var locationBar = document.querySelector(".location-bar");
-  var tabs = document.querySelector(".tabs");
+  var tabsContainer = document.querySelector(".tabs");
   var addTabButton = document.querySelector(".add-tab");
+  var contentContainer = document.querySelector(".content");
+
+  var tabs = document.querySelectorAll(".tab");
+
+  var newTab = function() {
+    var tab = document.createElement("div");
+    tab.classList.add("tab");
+    tab.classList.add("new-tab");
+    tabsContainer.insertBefore(tab, tabsContainer.firstChild);
+
+    // There should be tab slide animation, but it's not working.
+    window.setTimeout(function() {
+      contentContainer.classList.add("home");
+      toggleTabs();
+    }, 100);
+  };
 
   var toggleTabs = function() {
     locationBar.classList.toggle("small");
-    tabs.classList.toggle("hidden");
+    tabsContainer.classList.toggle("hidden");
     openTabButton.classList.toggle("hidden");
     menuButton.classList.toggle("hidden");
     addTabButton.classList.toggle("hidden");
@@ -15,12 +31,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Event listeners
 
-  openTabButton.addEventListener("click", function() {
-    toggleTabs();
-  });
+  openTabButton.addEventListener("click", toggleTabs);
 
   locationBar.addEventListener("click", function() {
     if(locationBar.classList.contains("small")) {
+      toggleTabs();
+    }
+  });
+
+  addTabButton.addEventListener("click", newTab);
+
+  tabsContainer.addEventListener("click", function(event) {
+    var classList =  event.target.classList;
+
+    if(classList.contains("new-tab")) {
+      contentContainer.classList.add("home");
+      toggleTabs();
+    }
+    else if(classList.contains("tab")) {
+      contentContainer.classList.remove("home");
+      toggleTabs();
+    }
+    else if(classList.contains("tabs")) {
       toggleTabs();
     }
   });
